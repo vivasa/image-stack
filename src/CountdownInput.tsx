c81be33f81dabcea728d6540325c356a4ad52934
Harikishore Tadigotla <harikishore@vivasa.in>
2023-06-19 17:26:00 +0530

    Vertical Slider

--- src/CountdownInput.tsx
+++ src/CountdownInput.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import ReactSlider from 'react-slider';
 import clsx from 'clsx';
+import './CountdownInput.css';
 
 interface CountdownInputProps {
   countdown: number;
@@ -10,15 +12,14 @@ interface CountdownInputProps {
 }
 
 const CountdownInput: React.FC<CountdownInputProps> = ({ countdown, setCountdown, className }) => {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = Number(e.target.value);
+  const handleChange = (value: number) => {
     setCountdown(value * 60); // converting minutes to seconds
   }
 
   const percentage = (countdown / (6 * 60)) * 100; // convert to percentage
 
   return (
-    <div className={clsx("flex items-center w-full space-x-5 p-5 bg-gray-100 rounded-lg shadow-md", className)}>
+    <div className={clsx("flex items-center space-x-5 p-5 bg-gray-100 rounded-lg shadow-md", className)}>
       <div style={{width: 80, height: 80}}> {/* Adjust size here */}
         <CircularProgressbar 
           value={percentage} 
@@ -31,14 +32,16 @@ const CountdownInput: React.FC<CountdownInputProps> = ({ countdown, setCountdown
           })}
         />
       </div>
-      <input 
-        type="range" 
-        className="slider flex-grow appearance-none bg-purple-500 height-1 rounded-full cursor-pointer"
-        min={30 / 60} // 30 seconds
-        max={360 / 60} // 360 seconds
-        step={1 / 60} // 1 second
-        value={countdown / 60} 
-        onChange={handleChange} 
+      <ReactSlider
+        className="vertical-slider"
+        thumbClassName="thumb"
+        trackClassName="track"
+        orientation="vertical"
+        min={30 / 60}
+        max={360 / 60}
+        step={1 / 60}
+        value={countdown / 60}
+        onChange={handleChange}
       />
     </div>
   )
